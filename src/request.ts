e70fff87d8e9106a50989218a73681b89606845b
Steve Lund <lund5000@gmail.com>
2020-06-22 09:57:24 -0400

    Update request.ts

--- src/request.ts
+++ src/request.ts
@@ -730,6 +730,8 @@ class DatastoreRequest {
    *   });
    */
   runQueryStream(query: Query, options: RunQueryStreamOptions = {}): Transform {
+    console.trace();
+    console.log('runQueryStream', options);
     query = extend(true, new Query(), query);
 
     const makeRequest = (query: Query) => {
